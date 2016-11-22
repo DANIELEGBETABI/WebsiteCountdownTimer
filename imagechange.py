d7215f1de397c43bafe0af725c3ab4c552d5e54c
Kyle Polley <kpolley@outlook.com>
2016-11-22 01:25:58 -0800

    Changed images to .png so orientation is correct. Minor fix to imagemover script

--- imagechange.py
+++ imagechange.py
@@ -6,18 +6,18 @@ from random import randint
 
 date = datetime.now()
 
-src = "/var/www/html/timer/image/image.jpg"
-dst = "/home/pi/Documents/TimerPhotos/Used/used " + str(date.strftime("%Y-%m-%d")) + ".jpg"
+src = "/var/www/html/timer/image/image.png"
+dst = "/home/pi/Documents/TimerPhotos/Used/used " + str(date.strftime("%Y-%m-%d")) + ".png"
 shutil.move(src, dst)
 
 
 images = [f for f in listdir("/home/pi/Documents/TimerPhotos") if isfile(join("/home/pi/Documents/TimerPhotos", f))]
 
 
-luckyImage = randint(1,len(images))
+luckyImage = randint(0,len(images))
 
 src = "/home/pi/Documents/TimerPhotos/" + images[luckyImage]
-dst = "/var/www/html/timer/image/image.jpg"
+dst = "/var/www/html/timer/image/image.png"
 shutil.move(src, dst)
 
 print("Moved " + images[luckyImage])
